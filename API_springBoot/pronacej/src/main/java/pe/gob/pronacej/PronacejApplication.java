b03b605bab4c996351ab820e3dca41d11399a814
Carlitos <carlitos@gmail.com>
2024-05-06 21:23:18 -0500

    Creatign directories

---
+++ API_springBoot/pronacej/src/main/java/pe/gob/pronacej/PronacejApplication.java
@@ -0,0 +1,13 @@
+package pe.gob.pronacej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronacejApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PronacejApplication.class, args);
+    }
+
+}
---
+++ API_springBoot/pronacej/src/test/java/pe/gob/pronacej/PronacejApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.gob.pronacej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PronacejApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
