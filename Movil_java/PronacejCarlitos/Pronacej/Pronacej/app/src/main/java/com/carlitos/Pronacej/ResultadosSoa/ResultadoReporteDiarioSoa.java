f8b65d3a320002ca6f86ba175fcfff3e579bdc35
Carlitos <carlitos@gmail.com>
2024-06-19 13:23:41 -0500

    Subiendo cambios antes de otros cambios

--- Movil_java/PronacejCarlitos/Pronacej/Pronacej/app/src/main/java/com/carlitos/Pronacej/ResultadosSoa/ResultadoReporteDiarioSoa.java
+++ Movil_java/PronacejCarlitos/Pronacej/Pronacej/app/src/main/java/com/carlitos/Pronacej/ResultadosSoa/ResultadoReporteDiarioSoa.java
@@ -29,8 +29,8 @@ import java.util.HashMap;
 public class ResultadoReporteDiarioSoa extends AppCompatActivity {
 
     private HorizontalBarChart barChart;
-    private TextView[] textViewsPorcentaje = new TextView[10];
-    private TextView[] textViewsNombre = new TextView[10];
+    private TextView[] textViewsPorcentaje = new TextView[26];
+    private TextView[] textViewsNombre = new TextView[26];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,22 @@ public class ResultadoReporteDiarioSoa extends AppCompatActivity {
         ArrayList<HashMap<String, String>> reportData = (ArrayList<HashMap<String, String>>) getIntent().getSerializableExtra("reportData");
 
         // Referencias a los TextView de porcentaje
+        textViewsPorcentaje[25] = findViewById(R.id.textViewTrujilloPorcentaje3);
+        textViewsPorcentaje[24] = findViewById(R.id.textViewJose_QuinonesPorcentaje3);
+        textViewsPorcentaje[23] = findViewById(R.id.textViewMiguel_GrauPorcentaje3);
+        textViewsPorcentaje[22] = findViewById(R.id.textViewSanta_MargaritaPorcentaje3);
+        textViewsPorcentaje[21] = findViewById(R.id.textViewAnexoIIIPorcentaje3);
+        textViewsPorcentaje[20] = findViewById(R.id.textViewLimaPorcentaje3);
+        textViewsPorcentaje[19] = findViewById(R.id.textViewAlfonsoUgartePorcentaje2);
+        textViewsPorcentaje[18] = findViewById(R.id.textViewMarcavallePorcentaje2);
+        textViewsPorcentaje[17] = findViewById(R.id.textViewPucallpaPorcentaje2);
+        textViewsPorcentaje[16] = findViewById(R.id.textViewEl_TamboPorcentaje2);
+        textViewsPorcentaje[15] = findViewById(R.id.textViewTrujilloPorcentaje2);
+        textViewsPorcentaje[14] = findViewById(R.id.textViewJose_QuinonesPorcentaje2);
+        textViewsPorcentaje[13] = findViewById(R.id.textViewMiguel_GrauPorcentaje2);
+        textViewsPorcentaje[12] = findViewById(R.id.textViewSanta_MargaritaPorcentaje2);
+        textViewsPorcentaje[11] = findViewById(R.id.textViewAnexoIIIPorcentaje2);
+        textViewsPorcentaje[10] = findViewById(R.id.textViewLimaPorcentaje2);
         textViewsPorcentaje[9] = findViewById(R.id.textViewAlfonsoUgartePorcentaje);
         textViewsPorcentaje[8] = findViewById(R.id.textViewMarcavallePorcentaje);
         textViewsPorcentaje[7] = findViewById(R.id.textViewPucallpaPorcentaje);
@@ -55,6 +71,22 @@ public class ResultadoReporteDiarioSoa extends AppCompatActivity {
         textViewsPorcentaje[0] = findViewById(R.id.textViewLimaPorcentaje);
 
         // Referencias a los TextView de nombres
+        textViewsNombre[25] = findViewById(R.id.textViewTrujillo3);
+        textViewsNombre[24] = findViewById(R.id.textViewJose_Quinones3);
+        textViewsNombre[23] = findViewById(R.id.textViewMiguel_Grau3);
+        textViewsNombre[22] = findViewById(R.id.textViewSanta_Margarita3);
+        textViewsNombre[21] = findViewById(R.id.textViewAnexoIII3);
+        textViewsNombre[20] = findViewById(R.id.textViewLima3);
+        textViewsNombre[19] = findViewById(R.id.textViewAlfonso_Ugarte2);
+        textViewsNombre[18] = findViewById(R.id.textViewMarcavalle2);
+        textViewsNombre[17] = findViewById(R.id.textViewPucallpa2);
+        textViewsNombre[16] = findViewById(R.id.textViewEl_Tambo2);
+        textViewsNombre[15] = findViewById(R.id.textViewTrujillo2);
+        textViewsNombre[14] = findViewById(R.id.textViewJose_Quinones2);
+        textViewsNombre[13] = findViewById(R.id.textViewMiguel_Grau2);
+        textViewsNombre[12] = findViewById(R.id.textViewSanta_Margarita2);
+        textViewsNombre[11] = findViewById(R.id.textViewAnexoIII2);
+        textViewsNombre[10] = findViewById(R.id.textViewLima2);
         textViewsNombre[9] = findViewById(R.id.textViewAlfonso_Ugarte);
         textViewsNombre[8] = findViewById(R.id.textViewMarcavalle);
         textViewsNombre[7] = findViewById(R.id.textViewPucallpa);
@@ -72,18 +104,18 @@ public class ResultadoReporteDiarioSoa extends AppCompatActivity {
         // Calcular la población total
         float poblacionTotal = 0;
         for (HashMap<String, String> data : reportData) {
-            poblacionTotal += Float.parseFloat(data.get("poblacion_cjdr"));
+            poblacionTotal += Float.parseFloat(data.get("poblacion_soa"));
         }
 
         // Asignar los datos a los TextView y crear las entradas del gráfico
         for (int i = 0; i < reportData.size(); i++) {
             HashMap<String, String> data = reportData.get(i);
-            float poblacion = Float.parseFloat(data.get("poblacion_cjdr"));
+            float poblacion = Float.parseFloat(data.get("poblacion_soa"));
             float porcentaje = (poblacion / poblacionTotal) * 100;
 
             entries.add(new BarEntry(i, poblacion));
             textViewsPorcentaje[i].setText(String.format("%.2f%%", porcentaje));
-            textViewsNombre[i].setText(data.get("centro_cjdr"));
+            textViewsNombre[i].setText(data.get("centro_soa"));
         }
 
         ArrayList<Integer> colors = new ArrayList<>();
@@ -118,7 +150,7 @@ public class ResultadoReporteDiarioSoa extends AppCompatActivity {
         barChart.getXAxis().setValueFormatter(new ValueFormatter() {
             @Override
             public String getFormattedValue(float value) {
-                return reportData.get((int) value).get("centro_cjdr");
+                return reportData.get((int) value).get("centro_soa");
             }
         });
         barChart.getXAxis().setGranularity(1f);
